9c13f069e521ced055d0c5931126968d272bfe0c
dejanV91 <dejanvukoicic91@gmail.com>
2022-12-16 17:29:25 +0100

    Finish display stories

--- src/Stories.js
+++ src/Stories.js
@@ -3,11 +3,28 @@ import React from "react";
 import { useGlobalContext } from "./context";
 
 const Stories = () => {
-  const { isLoading } = useGlobalContext();
+  const { isLoading, hits } = useGlobalContext();
   if (isLoading) {
     return <div className="loading"></div>;
   }
-  return <h2>stories component</h2>;
+  return (
+    <section className="stories">
+      {hits.hits.map((story) => {
+        const { objectID, title, num_comments, points, author } = story;
+        return (
+          <article key={objectID} className="story">
+            <h4 className="title">{title}</h4>
+            <p className="info">
+              {points} points by{" "}
+              <span>
+                {author} | {num_comments} comments
+              </span>
+            </p>
+          </article>
+        );
+      })}
+    </section>
+  );
 };
 
 export default Stories;
